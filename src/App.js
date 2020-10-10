18c1e1a3fd830a60cd49c6bfa843dd8e0f1ab7e0
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-10-10 22:36:32 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EditorJs from '@editorjs/editorjs';
+import Header from "@editorjs/editorjs/dist/editor/header"
+// import Header from '@editorjs/editorjs';
+import List from "@editorjs/editorjs/dist/editor/list"
+// import Embed from "@editorjs/embed"
+
 
 function App() {
+  let editor = new EditorJs({
+    holder: 'editorjs',
+    
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar:['link']
+      },
+      list: {
+        class: List,
+        inlineToolbar: [
+          'link',
+          'bold'
+        ]
+      },
+      embed: {
+        // class: Embed,
+        inlineToolbar: false,
+        config: {
+          services: {
+            youtube: true,
+            coub: true
+          }
+        }
+      }
+    }
+  });
+  
+  function save() {
+    editor.save().then((outputData) => {
+      console.log('Article data: ', outputData)
+    }).catch((error) => {
+      console.log('Saving failed: ', error)
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header className="app-header"></header>
+      <section>
+        <div className="editorjs" id="editorjs"></div>
+        <button onClick={save} >Save</button>
+      </section>
     </div>
   );
 }
